98bb83a30753b7e02cbc79c6c21b8f84cd8cb4f6
Miłosz Zając <milosz.k.zajac@gmail.com>
2023-11-05 20:57:01 +0100

    improved Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,18 +1,20 @@
+import reactImg from "./assets/react-core-concepts.png";
+
 export default function Header() {
   function genRandomInt(max) {
     return Math.floor(Math.random() * (max + 1));
   }
 
   const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-  let reactDescription = reactDescriptions[genRandomInt()];
+  const description = reactDescriptions[genRandomInt(2)];
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescription} React concepts you will need for almost any app you
-        are going to build!
+        {description} React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
   );
