7a1aa2475a12ed996ad76bcbf274950cee92ccca
Miłosz Zając <milosz.k.zajac@gmail.com>
2024-01-15 20:15:24 +0100

    Currentyly working

--- src/app/Main/CoreConcepts/CoreConcepts.jsx
+++ src/app/Main/CoreConcepts/CoreConcepts.jsx
@@ -2,12 +2,12 @@ import { CORE_CONCEPTS } from "./CORE_CONCEPTS.js";
 import "./coreConcepts.css";
 
 export default function CoreConcepts() {
-  const coreConcepts = CORE_CONCEPTS.map((concept) => {
+  const coreConcepts = CORE_CONCEPTS.map((concept) => (
     <li key={concept.id}>
       <img src={concept.image} alt={concept.title} />
       <h3>{concept.title}</h3>
       <p>{concept.description}</p>
-    </li>;
-  });
+    </li>
+  ));
   return <ul>{coreConcepts}</ul>;
 }
